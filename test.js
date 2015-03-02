3509e48fba2abd023aac7a05dca1fb4126fa0dfc
Nick Duncan <nick.b.duncan@gmail.com>
2015-03-01 21:41:47 -0800

    Added some formatting to the table, a main "index" page that has links that will (eventually) link to different types of searches a user can do on the database.

--- test.js
+++ test.js
@@ -1,18 +1,20 @@
 var p = require('path');
 var req = require('request');
 var mysql = require('mysql');
-console.log(p.join(process.cwd(), 'static', '/index.html'));
 var conn = mysql.createConnection({
 host : "localhost",
 user : "root",
 password : "",
-database : "cape_codd"
+database : "db_project"
 });
-var queryString = "Select * from INVENTORY";
-
+var queryString = "";
 $("form").submit(function( event ) {
 var arr = $('form').serializeArray();
-queryString = "Select " + arr[0].value +", " + arr[1].value + " from INVENTORY";
+queryString = "Select * " +
+			"from REVIEW " +
+			"where REVIEW.BusinessName = \'" + arr[0].value + "\'" +
+			" AND REVIEW.Rating >= " + arr[1].value + ";";
+	console.log(queryString);
 conn.query(queryString, function(error, results)
 {
 	if(error)
@@ -24,14 +26,17 @@ conn.query(queryString, function(error, results)
 			for (var col in results[0]) {
 				myTable+="<th>" + col + "</th>"; 
 			} 
-		myTable+='</tr></thead><tbody><tr>'
+		myTable+='</tr></thead><tbody><tr>';
 			for (var i = 0; i < results.length; i++) {
 				myTable+="</tr><tr>";
 				for (var col in results[i]) {
 				myTable+="<td>" + results[i][col] + "</td>";
 				} }
 		myTable+="</tr></tbody>";
+		document.getElementById('thetable').className = 'sortable CSSTableGenerator';
 		document.getElementById('thetable').innerHTML = myTable;
+		var thenewtable = document.getElementById('thetable');
+		sorttable.makeSortable(thenewtable);
 	}
 });
 event.preventDefault();
